e5fd1f5356b373d6c4fcc443a9b1c2a02c184bda
Fabian Perez <145504952+Jeffholgerburg@users.noreply.github.com>
2023-11-27 19:24:54 -0800

    what i did
    I have made some things made scrips for my project and other thing but almost finished

--- RubysAdventurePerez/Assets/scripts/rubycontoler.cs
+++ RubysAdventurePerez/Assets/scripts/rubycontoler.cs
@@ -5,27 +5,43 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
+
 public class NewBehaviourScript : MonoBehaviour
 {
+    public int maxHealth = 5;
+
+    public int currentHealth;
+    public float timeInvincible
+    public int health { get { return currentHeath; } ]
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-       QualitySettings.vSyncCount = 0;
-       Application.targetFrameRate = 10;  
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
+        void FixedUpdate();
+
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime; ;
 
-        transform.position = position;
-        
-
+        rigidbody2d.MovePosition(position);
     }
+    void ChangeHealth(int amount)
+
+       currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+
 }
